83f5fa95f1b5c19d251e039d4e9d9db42f7f1bc2
hmschlng <soulb9@naver.com>
2025-01-23 04:21:04 +0900

    feat: 생성자 Autowired 방식으로 수정

--- src/main/java/com/example/goods/controller/GoodsRestController.java
+++ src/main/java/com/example/goods/controller/GoodsRestController.java
@@ -2,7 +2,7 @@ package com.example.goods.controller;
 
 import com.example.goods.dto.GoodsDto;
 import com.example.goods.service.GoodsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
  * -----------------------------------------------------------
  * 2025. 1. 12.        doong2s       최초 생성
  */
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/goods")
 public class GoodsRestController {
 
-    @Autowired
-    private GoodsService goodsService;
+    private final GoodsService goodsService;
 
     @GetMapping("/{goodsNo}")
     public ResponseEntity<GoodsDto> getGoods(@PathVariable String goodsNo) {
